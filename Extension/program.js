26613cbc2246dfbd12c5de9dfe06a7aff1ae9eda
saithevi7 <cyruspradhan15@gmail.com>
2022-02-02 00:47:29 -0500

    .

---
+++ Extension/program.js
@@ -0,0 +1,9 @@
+let robot_image = [
+    "https://media.istockphoto.com/photos/ai-robot-thinking-picture-id1029035836?b=1&k=20&m=1029035836&s=170667a&w=0&h=QlRcVI9lRB2Mqz4BdGjFtHAlONeHacCBbZGrcr49a-c=",
+    "https://cdn1.vectorstock.com/i/1000x1000/33/05/cute-robot-vector-6263305.jpg",
+    "https://images.stockfreeimages.com/1803/sfi226w/18037025.jpg",
+    "https://www.google.com/url?sa=i&url=http%3A%2F%2Fclipart-library.com%2Frobot-images-free.html&psig=AOvVaw0cgvwJUTl8ysUTf6layqZx&ust=1643866509471000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDm2oym4PUCFQAAAAAdAAAAABAa",
+    "https://www.google.com/url?sa=i&url=http%3A%2F%2Fclipart-library.com%2Frobot-images-free.html&psig=AOvVaw0cgvwJUTl8ysUTf6layqZx&ust=1643866509471000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDm2oym4PUCFQAAAAAdAAAAABAl",
+    "https://thumbs.dreamstime.com/z/toy-robot-22558.jpg",
+    
+];
